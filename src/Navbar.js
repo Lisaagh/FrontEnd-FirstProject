9a147d76bc5ed63eba9728db87ec85df5bfeb430
Carlos <131988778+JCLV2002@users.noreply.github.com>
2024-11-05 19:24:43 -0700

    Diseño final

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#inicio">Inicio</a></li>
+                <li><a href="#servicios">Servicios</a></li>
+                <li><a href="#contacto">Contacto</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
---
+++ src/card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Card() {
+    return (
+        <div className="card">
+            <h2>Bienvenidos</h2>
+            <p>Mi primera pagina Web con react</p>
+        </div>
+    );
+}
+
+export default Card;
